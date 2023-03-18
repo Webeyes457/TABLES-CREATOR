a6c44ed9e7b940ba736a98e4ff5d6c697dcf6836
Muhammad Murtaza <121543125+Webeyes457@users.noreply.github.com>
2023-03-18 12:07:22 +0500

    Add files via upload

---
+++ PRINT-MULTIPLICATION-TABLE/app.js
@@ -0,0 +1,23 @@
+
+
+//  create variable and take input number from user
+
+var num=+prompt("ENTER A NUMBER : ")
+
+// create loop and built logic
+
+for(var i=1;i<=10;i++){
+
+    
+document.write("<p>")
+
+    document.write("<div>")
+    
+     
+     document.write(num + "  x  " + i + "  =  " + i*num)
+
+
+    document.write("</div>")
+
+    document.write("</p>")
+}
